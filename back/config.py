36d1c222f9412cf10937880d7adcc2da6d689e62
Manh Hung <manhhung20033004@gmail.com>
2024-12-17 16:03:20 +0000

    adding 1

--- back/config.py
+++ back/config.py
@@ -3,7 +3,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY")
     MONGO_URI = os.getenv("MONGO_URI")
--- back/routes/auth_routes.py
+++ back/routes/auth_routes.py
@@ -10,7 +10,7 @@ import os
 JWT_SECRET = os.getenv("JWT_SECRET")
 
 auth_blueprint = Blueprint("auth", __name__)
-CORS(auth_blueprint, origins=["http://localhost:3000"], supports_credentials=True)
+CORS(auth_blueprint, origins=["http://127.0.0.1:3000"], supports_credentials=True)
 
 @auth_blueprint.route("/login", methods=["POST"])
 def login():
